0a2aac50010ec4ac884ce17f4629d476b161fe8a
RicardsAlmanis <you@example.com>
2025-11-06 10:41:16 +0200

    3_uzd

---
+++ ticket-price.js
@@ -0,0 +1,9 @@
+let vistorAge = 12;
+let isStudent = true;
+if(vistorAge <= 6){
+    console.log("bez maksas")
+} else if(isStudent == true){
+    console.log("cena ir 5 eiro")
+} else {
+    console.log("cena ir 10 eiro")
+}
